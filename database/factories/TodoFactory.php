bac6a52a3d2757017f83ac114cea3a4a651a6931
Mirko <mirko.lorusso@gmail.com>
2022-01-04 23:32:28 +0100

    Add `TodoFactory`

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * Class TodoFactory.
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'caption' => $this->faker->sentence(),
+        ];
+    }
+}
