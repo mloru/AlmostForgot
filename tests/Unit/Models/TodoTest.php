c6dbd5eea8a22f79f4807ab5b360f2e61bad3ec5
Mirko <mirko.lorusso@gmail.com>
2022-01-05 00:03:10 +0100

    Add test for `Todo` model

---
+++ tests/Unit/Models/TodoTest.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+/**
+ * Class TodoTest.
+ */
+class TodoTest extends TestCase
+{
+    /**
+     * The number of models to persist for tests.
+     *
+     * @var int
+     */
+    const TODO_NUMBER = 10;
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function it_can_be_persisted(): void
+    {
+        /** @var Todo[] $todos */
+        $todos = Todo::factory(self::TODO_NUMBER)
+                     ->create();
+
+        $this->assertDatabaseCount(
+            Todo::class,
+            self::TODO_NUMBER
+        );
+
+        /** @var Todo $todo */
+        foreach($todos as $todo) {
+            $todo->refresh();
+
+            $this->assertNotNull($todo->id);
+            $this->assertIsInt($todo->id);
+            $this->assertGreaterThan(0, $todo->id);
+
+            $this->assertNotNull($todo->caption);
+            $this->assertIsString($todo->caption);
+            $this->assertNotEmpty($todo->caption);
+
+        }
+    }
+}
