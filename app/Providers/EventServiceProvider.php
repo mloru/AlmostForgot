78595770abcd396b0bc1c9a5aaec88cb0c7af42a
Mirko <mirko.lorusso@gmail.com>
2022-01-06 00:37:05 +0100

    Fix style

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,9 +5,8 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
-class EventServiceProvider extends ServiceProvider
+final class EventServiceProvider extends ServiceProvider
 {
     /**
      * The event listener mappings for the application.
