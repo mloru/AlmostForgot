0475fbc2b7a1327aa47bfcda32ffd85a150103ee
Mirko <mirko.lorusso@gmail.com>
2022-01-06 00:13:39 +0100

    Update `check-styles` script

--- build/check-styles.sh
+++ build/check-styles.sh
@@ -1,5 +1,7 @@
 echo "\nphpmd:\n" &&
     vendor/bin/phpmd app,bootstrap,config,database,routes,tests text phpmd.xml && # can produce this issue: https://github.com/phpmd/phpmd/issues/853
+    echo "\nphp-cs-fixer:\n" &&
+    vendor/bin/php-cs-fixer fix --verbose --no-interaction --dry-run --diff --stop-on-violation &&
     echo "\nphpstan:\n" &&
     vendor/bin/phpstan analyse --no-interaction --memory-limit=1G &&
     echo "\npsalm:\n" &&
