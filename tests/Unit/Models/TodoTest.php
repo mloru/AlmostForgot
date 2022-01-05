236e2e4978faa10921f309f4399816ed6979bd9c
Mirko <mirko.lorusso@gmail.com>
2022-01-05 22:32:35 +0100

    Fix `TodoTest`

--- tests/Unit/Models/TodoTest.php
+++ tests/Unit/Models/TodoTest.php
@@ -1,10 +1,9 @@
 <?php declare(strict_types=1);
 
-namespace Tests\Feature;
+namespace Tests\Unit\Models;
 
 use App\Models\Todo;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 /**
