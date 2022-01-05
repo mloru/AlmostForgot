e1e59948dae6c258168b57a468f1ae865f971000
Mirko <mirko.lorusso@gmail.com>
2022-01-05 23:21:08 +0100

    Update `check-styles` script

--- build/check-styles.sh
+++ build/check-styles.sh
@@ -1,4 +1,4 @@
-
-vendor/bin/phpstan analyse --no-interaction --memory-limit=1G &&
+vendor/bin/phpmd app,bootstrap,config,database,routes,tests text phpmd.xml && # can produce this issue: https://github.com/phpmd/phpmd/issues/853
+  vendor/bin/phpstan analyse --no-interaction --memory-limit=1G &&
   vendor/bin/psalm # can produce this issue: https://github.com/psalm/psalm-plugin-laravel/issues/145
 
