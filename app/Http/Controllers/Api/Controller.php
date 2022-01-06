9cc03bb5baa900ff0772662eb70c5f78feefcdf1
Mirko <mirko.lorusso@gmail.com>
2022-01-07 00:00:01 +0100

    Add API controllers structure

---
+++ app/Http/Controllers/Api/Controller.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller as BaseController;
+
+abstract class Controller extends BaseController
+{
+}
---
+++ app/Http/Controllers/Api/v1/Controller.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Api\Controller as BaseController;
+
+abstract class Controller extends BaseController
+{
+}
