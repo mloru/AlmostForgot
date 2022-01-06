b440b57af53cc308a2ac1077f141bb5bbdd324c1
Mirko <mirko.lorusso@gmail.com>
2022-01-06 02:22:55 +0100

    Generate `laravel-ide-helper` PHPDoc for models

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -9,6 +9,19 @@ use Illuminate\Database\Eloquent\Model;
  * Class Todo.
  *
  * @final
+ * @property int $id
+ * @property string $caption
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\TodoFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereCaption($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 final class Todo extends Model
 {
