9c0fb56826bfc13a072186a4a2defb5733b0701a
Mirko <mirko.lorusso@gmail.com>
2022-01-09 17:10:38 +0100

    Add TODO comment

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->group(function (): void {
+    // TODO: metterle sotto autenticazione
     Route::resource('todos', TodoController::class)->only([
         'index',
     ]);
