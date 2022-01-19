9125338e2e4a6a27a11b01c37f7cca1e00d2f645
Mirko <mirko.lorusso@gmail.com>
2022-01-20 00:18:29 +0100

    Fix wrong trait imported in TestCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,6 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Orchestra\Testbench\Concerns\CreatesApplication;
 
 abstract class TestCase extends BaseTestCase
 {
